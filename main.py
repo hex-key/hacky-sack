a8652171538cfe0fb1af11eccce669432aa851a9
T-FWK <irischen0505@gmail.com>
2024-11-08 22:06:12 -0500

    asbdlfkjbalsbfw

--- main.py
+++ main.py
@@ -1,26 +1,58 @@
-# This example requires the 'message_content' intent.
+"""
+    SETUP
+"""
 
+# imports
 import discord
 import os
-from dotenv import load_dotenv()
+from tinydb import TinyDB, Query
+from discord.ext import commands
+from dotenv import load_dotenv
 
-load_dotenv()
+# set up json database
+db = TinyDB('db.json')
 
+
+# set up discord app intents and shit
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+
+
+
+"""
+    FUNCTIONALITY
+"""
+
+# EVENT LISTENERS
+
+# print to terminal when logged in
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'Logged in as {client.user}')
+
+
+# process every new message
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    msg = message.content
+
+# COMMANDS
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith("kys"):
+        await message.channel.send("no because it doesn't take being alive to love life")
+
+# run the guy
+load_dotenv()
 
 client.run(os.getenv('TOKEN'))
