0cc02cc7633845dff04c907783e077950af7ac0f
T-FWK <irischen0505@gmail.com>
2024-11-10 10:57:10 -0500

    last commit !

--- main.py
+++ main.py
@@ -77,10 +77,10 @@ async def on_message(message):
 
     for i in message.mentions:
         if i.id == client.user.id:
-            await message.channel.send("leave me alone. type `$help` for help")
+            await message.channel.send("type `$help` for help!")
 
-    if message.content.startswith("kys"):
-        await message.channel.send("no because it doesn't take being alive to love life")
+    if message.content.startswith("go"):
+        await message.channel.send("go umass!")
     
     if message.content.startswith("kms"):
         await message.channel.send("Nooo don't kill yourself your so sexy aha")
@@ -282,82 +282,65 @@ async def quiz_me(ctx, set_name="data"):
 
 # LANGUAGE IMMERSION
 
-# #return all terms for a language
-# @client.command()
-# async def all_words(ctx, lang_name):
-#     s_db = db.get(query.func == "study")
-#     if set_name in s_db.keys():
-#         all_terms = s_db[set_name]
-#         all_terms_string = "__**" + set_name.upper() + "**__\n"
-#         for key, value in all_terms.items():
-#             all_terms_string += f"`{key}`: {value}\n"
-#         await ctx.send(all_terms_string)
-#     else:
-#         await ctx.send("Set does not exist.")
+#return all terms for a language
+@client.command()
+async def all_words(ctx, lang_name):
+    s_db = db.get(query.func == "lang")[0]
+    if lang_name in s_db.keys():
+        all_terms = s_db[lang_name]
+        all_terms_string = "__**" + lang_name.upper() + "**__\n"
+        for key, value in all_terms.items():
+            all_terms_string += f"`{key}`: {value}\n"
+        await ctx.send(all_terms_string)
+    else:
+        await ctx.send("Language does not exist.")
 
-# # import set like from quizlet or something
-# @client.command()
-# async def import_lang(ctx, *, text):
-#     try:
-#         s_db = db.get(query.func == "study")
+# import set like from quizlet or something
+@client.command()
+async def import_lang(ctx, *, text):
+    try:
+        def check_reply(m):
+            return m.author == ctx.author and m.channel == ctx.channel
+        await ctx.send("Enter a two-character code for this language!")
+        msg = await client.wait_for("message", timeout=60.0, check=check_reply)
+        name = msg.content.lower()
+        while db.search(where(name).exists()):
+            await ctx.send("This language already exists, choose a new name or reply `cancel` and use add_term or delete_set.")
+            msg = await client.wait_for("message", timeout=60.0, check=check_reply)
+            name = msg.content
+            if name == "cancel":
+                await ctx.send("Set creation cancelled.")
+                return
 
-#         def check_reply(m):
-#             return m.author == ctx.author and m.channel == ctx.channel
-#         await ctx.send("Enter a two-character name for this set!")
-#         msg = await client.wait_for("message", timeout=60.0, check=check_reply)
-#         name = msg.content
-#         while db.search(where(name).exists()):
-#             await ctx.send("This set already exists, choose a new name or reply `cancel` and use add_term or delete_set.")
-#             msg = await client.wait_for("message", timeout=60.0, check=check_reply)
-#             name = msg.content
-#             if name == "cancel":
-#                 await ctx.send("Set creation cancelled.")
-#                 return
-
-#         study_set = {}
-#         linked_pairs = text.split(";")
-#         for linked_pair in linked_pairs:
-#             pair = linked_pair.split(",", 1)
-#             study_set[pair[0]] = pair[1]
+        study_set = {}
+        linked_pairs = text.split(";")
+        for linked_pair in linked_pairs:
+            pair = linked_pair.split(",", 1)
+            study_set[pair[0]] = pair[1]
         
-#         db.update({name: study_set}, query.func == "study")
-#         await ctx.send(f"Language set {name} created!")
-#     except commands.errors.MissingRequiredArgument:
-#         await ctx.send("Didn't see any text to import! (format: term1,def1;term2,def2; ...)")
-#     except asyncio.TimeoutError:
-#         await ctx.send(content="Study set creation timed out.", reference=ctx.message)
+        db.upsert({name: study_set}, query.func == "lang")
+        await ctx.send(f"Language set {name} created!")
+    except commands.errors.MissingRequiredArgument:
+        await ctx.send("Didn't see any text to import! (format: term1,def1;term2,def2; ...)")
+    except asyncio.TimeoutError:
+        await ctx.send(content="Language set creation timed out.", reference=ctx.message)
 
-# #ask questions from the flashcards!
-# @client.command()
-# async def quiz_me(ctx, set_name="data"):
-#     try:
-#         c_db = db.get(query.func == "study")
-#         s_list = c_db[set_name]
-#         print(s_list)
-#         num = len(s_list)
+# toggle enable or disable certain language
+@client.command()
+async def toggle(ctx, lang_code):
+    if db.search(where(lang_code).exists()):
+        global enabled_languages
+        if lang_code in enabled_languages:
+            enabled_languages.remove(lang_code)
+            db.update({"enabled":enabled_languages}, query.func == "lang")
+            await ctx.send("Deactivated prompts for `{lang_code}`.")
+        else:
+            enabled_languages.append(lang_code)
+            db.update({"enabled":enabled_languages}, query.func == "lang")
+            await ctx.send(f"Activated prompts for `{lang_code}`.")
+    else:
+        await ctx.send("Couldn't find that language!")
 
-#         correct = 0
-#         idxs = [i for i in range(num)]
-#         random.shuffle(idxs)
-#         for i in idxs:
-#             q = list(s_list.keys())[i]
-#             await ctx.send(q)
-#             def check_reply(m):
-#                 return m.author == ctx.author and m.channel == ctx.channel
-#             msg = await client.wait_for("message", timeout=30.0, check=check_reply)
-#             a = msg.content
-#             if (a == s_list[q]):
-#                 await ctx.send("Your answer was right!")
-#                 correct += 1
-#             else:
-#                 await ctx.send("You're a bum! The correct answer is " + s_list[q])
-#         await ctx.send(f"I have nothing left to ask... You got {correct} out of {num}")
-
-#     except KeyError:
-#         await ctx.send("Set does not exist.")
-#     except asyncio.TimeoutError:
-#         await ctx.send(content="You died rip", reference=ctx.message)
-        
 
 
 """
