8d1312ec986f86b8fcb488483b4595cdaec9222b
T-FWK <irischen0505@gmail.com>
2024-11-09 19:29:29 -0500

    the terms stuff sorta works
    Co-authored-by: rey <snooork@users.noreply.github.com>
    Co-authored-by: lncvyz <lncvyz@users.noreply.github.com>

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = commands.Bot(intents=intents, command_prefix='$', case_insensitive=True)
-
+client.remove_command("help")
 
 
 
@@ -71,7 +71,7 @@ async def on_message(message):
 
     for i in message.mentions:
         if i.id == client.user.id:
-            await message.channel.send("leave me alone")
+            await message.channel.send("leave me alone. type `$help` for help")
 
     if message.content.startswith("kys"):
         await message.channel.send("no because it doesn't take being alive to love life")
@@ -84,14 +84,27 @@ async def on_message(message):
 
     await client.process_commands(message)
 
-
-
-# COMMANDS
-
+#ping msg
 @client.command()
 async def ping(ctx):
     await ctx.send("pong! latency: " + str(client.latency))
 
+@client.command()
+async def flip(ctx):
+    sides = ["heads", "tails"]
+    flip_result = random.choice(sides)
+    await ctx.send(flip_result)
+
+#list of commands
+@client.command()
+async def help(ctx):
+    compliment_jar_commands = f"__**COMPLIMENT JAR**__\n`$add_compliment`: adds a compliment to the jar\n`$change_empty_message`: changes the message displayed when jar is empty\n`$compliment_me`: gives you a compliment from the jar\n\n"
+    study_commands = f"__**STUDYING**__\n`$add_term`: adds term to overall list of terms\n`$all_terms`: sends all terms\n`$import_set`: adds group of terms in their own subgroup"
+    await ctx.send(compliment_jar_commands + study_commands)
+
+# COMPLIMENT JAR
+
+#add compliment to "jar"
 @client.command()
 async def add_compliment(ctx, *, compliment:str):
     c_db = db.get(query.func == "compliments")
@@ -103,37 +116,55 @@ async def add_compliment(ctx, *, compliment:str):
 
     await ctx.send(f"added your compliment to the jar! there are {len(c_list)} compliments in the jar.")
 
+#retrieve compliment from "jar" and remove it
 @client.command()
 async def compliment_me(ctx):
     c_db = db.get(query.func == "compliments")
     c_list = c_db["data"]
     this_comp = c_db["when_empty"]
     if len(c_list) > 0:
-        this_comp = c_list.pop(random.randint(0, len(c_list) - 1))
+        index = random.randint(0, len(c_list)-1)
+        this_comp = c_list[index]
+        c_list = c_list[0:index] + c_list[index:]
         db.update({"data": c_list}, query.func == "compliments")
     await ctx.send(this_comp)
 
+#change default empty jar message
 @client.command()
 async def change_empty_message(ctx, *, message:str):
     db.update({"when_empty" : message}, query.func == "compliments")
     await ctx.send("empty jar message changed :)")
 
+
+
+# STUDYING
+
+#add new term to general database
 @client.command()
 async def add_term(ctx, *, study_term:str):
-    division_index = study_term.index(",")
-    term = study_term[0, division_index]
-    definition = study_term[division_index + 1, len(study_term)]
+    term, definition = study_term.split(",", 1)
     c_db = db.get(query.func == "study")
     s_list = c_db["data"]
-    newTerm = {term, definition}
-    s_list.append(newTerm)
+    s_list[term] = definition
     db.update({"data": s_list}, query.func == "study")
     await ctx.send("added new term: " + study_term)
 
+#return all terms
+@client.command()
+async def all_terms(ctx, set_name:str="data"):
+    s_db = db.get(query.func == "study")
+    if set_name in s_db.keys():
+        all_terms = s_db[set_name]
+        all_terms_string = ""
+        for key, value in all_terms.items():
+            all_terms_string += f"**{key}**: {value}\n"
+        await ctx.send(all_terms_string)
+    else:
+        await ctx.send("Set does not exist.")
 
+# import set like from quizlet or something
 @client.command()
 async def import_set(ctx, *, text):
-    
     try:
         s_db = db.get(query.func == "study")
 
@@ -162,16 +193,33 @@ async def import_set(ctx, *, text):
     except asyncio.TimeoutError:
         await ctx.send(content="Study set creation timed out.", reference=ctx.message)
 
-
-
-
+#ask questions from the flashcards?
 @client.command()
-async def flip(ctx):
-    sides = ["heads", "tails"]
-    flip_result = random.choice(sides)
-    await ctx.send(flip_result)
+async def quiz_me(ctx):
+    try:
+        c_db = db.get(query.func == "study")
+        s_list = c_db["data"]
+        while len(s_list) > 0:
+            q = random.choice(list(s_list.keys()))
+            await ctx.send(q)
+            msg = await client.wait_for("message", timeout=5.0)
+            a = msg.content
+            if (a==s_list[q]):
+                await ctx.send("Your answer was right!")
+            else:
+                await ctx.send("You're a bum! The correct answer is " + s_list[q])
+            del s_list[q]
+    except asyncio.TimeoutError:
+        await ctx.send(content="You died rip", reference=ctx.message)
+        
 
+
+
+
+
+"""
+    RUN
+"""
 # run the guy
 load_dotenv()
-
 client.run(os.getenv('TOKEN'))
