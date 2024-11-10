a590f0f9180a17ffc6fda2b8dfcc573a5697da64
T-FWK <irischen0505@gmail.com>
2024-11-10 10:10:47 -0500

    cook tarot and lang prelim

--- main.py
+++ main.py
@@ -21,8 +21,6 @@ from utils_lang import *
 db = TinyDB("db.json")
 query = Query()
 
-tarot_db = TinyDB("tarot.json")
-
 # set up discord app intents and shit
 intents = discord.Intents.default()
 intents.message_content = True
@@ -47,6 +45,13 @@ async def on_ready():
     
     if not db.search(query.func == "study"):
         db.insert({"func": "study", "data": {}})
+
+    if not db.search(query.func == "lang"):
+        db.insert({"func": "lang", "enabled": ["es"], "es": {"rojo": "red"}, "fr": {"rouge": "red"}})
+
+    global enabled_languages
+    enabled_languages = db.search(query.func == "lang")[0]["enabled"]
+
 
 
 
@@ -80,8 +85,32 @@ async def on_message(message):
     if message.content.startswith("kms"):
         await message.channel.send("Nooo don't kill yourself your so sexy aha")
 
-    if "something" in message.content:
-        pass
+    global enabled_languages
+    lang_db = db.search(query.func == "lang")[0]
+    possible_words = []
+
+    for word in msg.split():
+        for lang in enabled_languages:
+            lang_dict = lang_db[lang]
+            for key in lang_dict.keys():
+                if lang_dict[key] == word:
+                    possible_words.append((lang, word, key))
+    if len(possible_words) > 0:
+        trip = random.choice(possible_words)
+        await message.channel.send(f"Language check! What is `{trip[1]}` in {trip[0].upper()}? (5s)")
+        def check_reply(m):
+            return m.author == message.author and m.channel == message.channel
+        try:
+            m = await client.wait_for("message", timeout=5.0, check=check_reply)
+            if m.content.lower() == trip[2]:
+                await message.channel.send("You got it! :D")
+            else:
+                await message.channel.send(f"Not quite! The answer was: `{trip[2]}`")
+        except asyncio.TimeoutError:
+            await message.channel.send(f"Too slow! The answer was: `{trip[2]}`")
+        
+
+
 
     await client.process_commands(message)
 
@@ -101,9 +130,12 @@ async def flip(ctx):
 @client.command()
 async def tarot(ctx):
     #tarot_list = tarot_db["data"]
-    tarot_list = [ "Fool", "Magician", "High Priestess", "Empress", "Emperor", "Hierophant", "Lovers", "Chariot", "Strength", "Hermit", "Wheel of Fortune", "Justice", "Hanged Man", "Death", "Temperance", "Devil", "Tower", "Star", "Moon", "Sun", "Judgment", "World" ] 
-    this_card = random.choice(tarot_list)
-    await ctx.send(f"Your card is the {this_card} card!")
+    tarot_db = db.get(query.func == "tarot")
+
+    # tarot_list = [ "Fool", "Magician", "High Priestess", "Empress", "Emperor", "Hierophant", "Lovers", "Chariot", "Strength", "Hermit", "Wheel of Fortune", "Justice", "Hanged Man", "Death", "Temperance", "Devil", "Tower", "Star", "Moon", "Sun", "Judgment", "World" ] 
+    tarot_list = tarot_db["data"]
+    this_card = random.choice(list(tarot_list.keys()))
+    await ctx.send(f"Your card is the {this_card} card, which signifies {tarot_list[this_card]}!")
 
 #list of commands
 @client.command()
@@ -210,11 +242,12 @@ async def import_set(ctx, *, text):
         
         db.update({name: study_set}, query.func == "study")
         await ctx.send(f"Study set {name} created!")
-
+    except commands.errors.MissingRequiredArgument:
+        await ctx.send("Didn't see any text to import! (format: term1,def1;term2,def2; ...)")
     except asyncio.TimeoutError:
         await ctx.send(content="Study set creation timed out.", reference=ctx.message)
 
-#ask questions from the flashcards?
+#ask questions from the flashcards!
 @client.command()
 async def quiz_me(ctx, set_name="data"):
     try:
@@ -247,7 +280,84 @@ async def quiz_me(ctx, set_name="data"):
         
 
 
+# LANGUAGE IMMERSION
 
+# #return all terms for a language
+# @client.command()
+# async def all_words(ctx, lang_name):
+#     s_db = db.get(query.func == "study")
+#     if set_name in s_db.keys():
+#         all_terms = s_db[set_name]
+#         all_terms_string = "__**" + set_name.upper() + "**__\n"
+#         for key, value in all_terms.items():
+#             all_terms_string += f"`{key}`: {value}\n"
+#         await ctx.send(all_terms_string)
+#     else:
+#         await ctx.send("Set does not exist.")
+
+# # import set like from quizlet or something
+# @client.command()
+# async def import_lang(ctx, *, text):
+#     try:
+#         s_db = db.get(query.func == "study")
+
+#         def check_reply(m):
+#             return m.author == ctx.author and m.channel == ctx.channel
+#         await ctx.send("Enter a two-character name for this set!")
+#         msg = await client.wait_for("message", timeout=60.0, check=check_reply)
+#         name = msg.content
+#         while db.search(where(name).exists()):
+#             await ctx.send("This set already exists, choose a new name or reply `cancel` and use add_term or delete_set.")
+#             msg = await client.wait_for("message", timeout=60.0, check=check_reply)
+#             name = msg.content
+#             if name == "cancel":
+#                 await ctx.send("Set creation cancelled.")
+#                 return
+
+#         study_set = {}
+#         linked_pairs = text.split(";")
+#         for linked_pair in linked_pairs:
+#             pair = linked_pair.split(",", 1)
+#             study_set[pair[0]] = pair[1]
+        
+#         db.update({name: study_set}, query.func == "study")
+#         await ctx.send(f"Language set {name} created!")
+#     except commands.errors.MissingRequiredArgument:
+#         await ctx.send("Didn't see any text to import! (format: term1,def1;term2,def2; ...)")
+#     except asyncio.TimeoutError:
+#         await ctx.send(content="Study set creation timed out.", reference=ctx.message)
+
+# #ask questions from the flashcards!
+# @client.command()
+# async def quiz_me(ctx, set_name="data"):
+#     try:
+#         c_db = db.get(query.func == "study")
+#         s_list = c_db[set_name]
+#         print(s_list)
+#         num = len(s_list)
+
+#         correct = 0
+#         idxs = [i for i in range(num)]
+#         random.shuffle(idxs)
+#         for i in idxs:
+#             q = list(s_list.keys())[i]
+#             await ctx.send(q)
+#             def check_reply(m):
+#                 return m.author == ctx.author and m.channel == ctx.channel
+#             msg = await client.wait_for("message", timeout=30.0, check=check_reply)
+#             a = msg.content
+#             if (a == s_list[q]):
+#                 await ctx.send("Your answer was right!")
+#                 correct += 1
+#             else:
+#                 await ctx.send("You're a bum! The correct answer is " + s_list[q])
+#         await ctx.send(f"I have nothing left to ask... You got {correct} out of {num}")
+
+#     except KeyError:
+#         await ctx.send("Set does not exist.")
+#     except asyncio.TimeoutError:
+#         await ctx.send(content="You died rip", reference=ctx.message)
+        
 
 
 """
